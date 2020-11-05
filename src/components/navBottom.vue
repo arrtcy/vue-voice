a749c5e2ec5a855671c6dd6c9cab7e6ff9ac7478
arrtcy <2510356829@qq.com>
2020-11-05 11:10:42 +0800

    自动播放功能

--- src/components/navBottom.vue
+++ src/components/navBottom.vue
@@ -1,23 +1,23 @@
 <template>
   <!-- 底部播放 -->
-  <aplayer
-    autoplay
-    theme
-    id="aplayer"
-    :list="audio"
-    :showLrc="true"
-    :listForded="false"
-    listMaxHeight="100px"
-    :music="{
-      title: this.audio[0].title,
-      artist: '歌手: ' + this.audio[0].artist,
-      src: this.audio[0].src,
-      lrc: this.audio[0].lrc,
-      pic: this.audio[0].pic,
-    }"
-  >
-    <slot> </slot>
-  </aplayer>
+  <div class="aplay">
+    <aplayer
+      id="aplayer"
+      :list="audio"
+      :showLrc="true"
+      :listForded="false"
+      listMaxHeight="100px"
+      :music="{
+        title: this.audio[0].title,
+        artist: '歌手: ' + this.audio[0].artist,
+        src: this.audio[0].src,
+        lrc: this.audio[0].lrc,
+        pic: this.audio[0].pic,
+      }"
+    >
+    </aplayer>
+    <div class="top" @click="top"></div>
+  </div>
 </template>
 
 <script>
@@ -26,13 +26,6 @@ import { getlyric, getnear } from "../unitls/Serach";
 export default {
   data() {
     return {
-      obj: {
-        title: "情非得已",
-        artist: "田崇阳",
-        src: "/mp3/小F4 - 情非得已.mp3",
-        lrc: "[00:00.00]lrc here\n[00:01.00]aplayer",
-        pic: "/img/mv.png",
-      },
       audio: [
         {
           title: "情非得已",
@@ -62,28 +55,35 @@ export default {
       obj1.pic = v.al.picUrl;
       this.obj = obj1;
       this.audio.unshift(obj1);
-      document.querySelector(".aplayer-list-light")[0].click();
     },
     async loadnearSongs() {
       if (localStorage.getItem("uid")) {
-        let uid = localStorage.getItem("uid");
-        let res = await getnear(uid);
+        // let uid = localStorage.getItem("uid");
+        let res = await getnear();
         let arr = [];
         console.log(res);
         res.data.weekData.forEach((v) => {
           arr.push({
             id: v.song.id,
+            src: `https://music.163.com/song/media/outer/url?id=${v.song.id}.mp3`,
             title: v.song.name,
             pic: v.song.picUrl,
             artist: v.ar[0].name,
+          });
+          arr.forEach((v) => {
+            getlyric(v.id).then((res) => (v.lrc = res.data.lrc.lyric));
           });
           console.log(arr);
         });
       }
     },
+    top() {
+      console.log(2222);
+    },
   },
   mounted() {
-    // this.loadnearSongs();
+    this.loadnearSongs();
+    document.querySelector("audio").autoplay = true;
   },
 };
 </script>
@@ -98,6 +98,17 @@ export default {
 }
 #aplayer .aplayer-author {
   position: absolute;
-  left: 1rem;
+  right: 0;
+}
+.aplay {
+  position: relative;
+}
+.top {
+  position: absolute;
+  z-index: 9999;
+  height: 50px;
+  top: 20%;
+  left: 45%;
+  width: 30%;
 }
 </style>
